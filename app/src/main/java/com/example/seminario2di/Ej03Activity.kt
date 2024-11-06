2459b03ffd326241480d88a607832492ea0af6c4
memnochdavid <davduquediaz@gmail.com>
2024-11-06 15:06:28 +0100

    todo ok

--- app/src/main/java/com/example/seminario2di/Ej03Activity.kt
+++ app/src/main/java/com/example/seminario2di/Ej03Activity.kt
@@ -30,36 +30,13 @@ class Ej03Activity : AppCompatActivity() {
         semaforo = findViewById(R.id.sem)
 
         lifecycleScope.launch {
-            repeat(15) {//15 veces
                 cambia_color(R.drawable.semverde, 2000,) // verde
                 cambia_color(R.drawable.semrojo, 2000,)  // rojo
                 cambia_color(R.drawable.semambar, 1000,) // ambar
-            }
+
         }
-
-
-/*
-        var semaforo=findViewById<ImageView>(R.id.sem)
-        var stop=false
-        var cont=5
-
-        while(!stop){
-            if(verde(2,semaforo)){
-                if(rojo(2,semaforo)){
-                    if(ambar(1,semaforo)){
-                        continue
-                    }
-                }
-            }
-            cont--
-            if(cont==0){
-                stop=true
-            }
-        }
-*/
 
     }
-
 
     private suspend fun cambia_color(drawable: Int, temp: Long) {
         withContext(Dispatchers.Main) {
@@ -67,55 +44,5 @@ class Ej03Activity : AppCompatActivity() {
         }
         delay(temp)
     }
-/*
-    fun rojo(tiempo:Long,semaforo:ImageView):Boolean{
-        var temp=tiempo
-        var devuelve=false
-        object: CountDownTimer(temp*1000,1000){
-            override fun onTick(millisUntilFinished: Long) {
-                temp--
-            }
-            override fun onFinish() {
-                semaforo.setImageResource(R.drawable.semrojo)
-                devuelve=true
-            }
-        }.start()
-        return devuelve
-    }
-
-
-    fun verde(tiempo:Long,semaforo:ImageView):Boolean{
-        var temp=tiempo
-        var devuelve=false
-        var temp_verde=object: CountDownTimer(temp*1000,1000){
-            override fun onTick(millisUntilFinished: Long) {
-                temp--
-            }
-            override fun onFinish() {
-                semaforo.setImageResource(R.drawable.semverde)
-                devuelve=true
-            }
-        }
-        temp_verde.start()
-        return devuelve
-    }
-
-    fun ambar(tiempo:Long,semaforo:ImageView):Boolean{
-        var temp=tiempo
-        var devuelve=false
-        var temp_ambar=object: CountDownTimer(temp*1000,1000){
-            override fun onTick(millisUntilFinished: Long) {
-                temp--
-            }
-            override fun onFinish() {
-                semaforo.setImageResource(R.drawable.semambar)
-                devuelve=true
-            }
-        }
-        temp_ambar.start()
-        return devuelve
-    }
-
-*/
 
 }
